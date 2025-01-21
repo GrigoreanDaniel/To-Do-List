1f3b675914f54a5657516e430dd3b666c7365045
DarkLion61413 <75176773+GrigoreanDaniel@users.noreply.github.com>
2025-01-21 11:23:40 +0200

    Update WeatherForecast.cs

--- To-Do List/WeatherForecast.cs
+++ To-Do List/WeatherForecast.cs
@@ -4,7 +4,7 @@ namespace To_Do_List {
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 30 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
